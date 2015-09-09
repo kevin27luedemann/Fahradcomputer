e947376cc325873ee31f3706258df52dedb7f42a
kevin Luedemann <kevin27luedemann@googlemail.com>
2015-09-09 10:53:30 +0200

    datein hinzugefuegt

---
+++ Programm/Fahradcomputer/LSM303D.cpp
@@ -0,0 +1,19 @@
+/* 
+* LSM303D.cpp
+*
+* Created: 09.09.2015 08:54:18
+* Author: kevin
+*/
+
+
+#include "LSM303D.h"
+
+// default constructor
+LSM303D::LSM303D()
+{
+} //LSM303D
+
+// default destructor
+LSM303D::~LSM303D()
+{
+} //~LSM303D
