a3345bb0215e2c1e523e46a70173c50939df88b1
kevin27luedemann <kevin27luedemann@googlemail.com>
2015-10-09 08:21:34 +0200

    Glurak hinzu

--- Pokedex/Bildertest.py
+++ Pokedex/Bildertest.py
@@ -8,7 +8,7 @@ Created on Tue Oct  6 18:47:01 2015
 from PIL import Image
 
 #Bild laden
-im = Image.open("025.png")
+im = Image.open("006.png")
 pix = im.load()
 x = im.size
 assert x[0]==64, "Bild falsche Groese"
@@ -16,17 +16,17 @@ assert x[0]==64, "Bild falsche Groese"
 print x
 
 #Ausgabedatei Oeffnen
-fout = open('Pikachu.h',"w")
+fout = open('006.h',"w")
 fout.write('/*\n')
-fout.write('\tHeader fuer Pixelmuster von Pikachu\n')
+fout.write('\tHeader fuer Pixelmuster von Glurak\n')
 fout.write('*/\n')
-fout.write('const uint8_t Pikachu[][64] PROGMEM = {\n')
+fout.write('const uint8_t Glurak[][64] PROGMEM = {\n')
 for i in range(0,8):
     fout.write('{')
     for j in range(0,x[1]):
         zahl=0
         for k in range(0,8):
-            if pix[j,i*8+k][0]!=0:
+            if pix[j,i*8+k][0]==0:
                zahl+=2**k
                print zahl
         if j!=x[1]-1:
